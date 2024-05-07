e108429fe29e7c5b6187938439514b006a8329b0
Kamal-Khadka101 <khadkasaroj112@gmail.com>
2024-05-07 23:20:38 +0545

    Completed the challenge
    The challenge was to update the application to have n updated view of the result. We introduced new variables and conducted a few mathematical operations to achieve the application's goal.

--- C# Padhum/Challenge-project-foreach-if-array-CSharp-main/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ C# Padhum/Challenge-project-foreach-if-array-CSharp-main/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289c4563d862c325fb2f01d0dbd463d9b7f6879c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4130dd8df547153421ca1b0d958e60c63554b7e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
