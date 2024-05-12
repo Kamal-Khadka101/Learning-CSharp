cbc233402a5b93299912f2d96c9a4baf2230a42f
Kamal-Khadka101 <khadkasaroj112@gmail.com>
2024-05-12 23:35:05 +0545

    Switch Cases in C#
    Learnt when to use if else and when to use switch cases and did a few problems based on switch cases.

---
+++ C# Padhum/Part3/Program.cs
@@ -0,0 +1,41 @@
+﻿// using System;
+//              Part 3 - Coin Flip Challenge
+// Random coinFlip = new Random();
+// int flipNum = coinFlip.Next(1,3);
+// string flipResult = flipNum == 1 ? "Heads":"Tails";
+// System.Console.WriteLine($"The result of coin flip is: {flipResult}");
+
+// string permission = "Admin|Manager";
+// int level = 53;
+// if (permission.Contains("Admin"))
+// {
+//     if (level > 55)
+//         System.Console.WriteLine("Welcome, Super Admin user.");
+//     else
+//         System.Console.WriteLine("Welcome, Admin user.");
+// }
+// else if (permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//         System.Console.WriteLine("Contact an Admin for access.");
+//     else
+//         System.Console.WriteLine("You do not have sufficient privileges.");
+// }
+// else
+//     System.Console.WriteLine("You do not have sufficient privileges.");
+//                    Booleans and accessing them from inside and outside code blocks
+// bool flag = true;
+// int value = 0;
+// if (flag){
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+//                  Removing code blocks from if statement
+// bool flag = true;
+// if (flag) Console.WriteLine(flag);
+
+// string name = "steve";
+// if (name == "bob") Console.WriteLine("Found Bob");
+// else if (name == "steve") Console.WriteLine("Found Steve");
+// else Console.WriteLine("Found Chuck");
---
+++ C# Padhum/VariableScope/Program.cs
@@ -0,0 +1,40 @@
+﻿//              Part 3 - Variable Scope
+// int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+// int total = 0;
+// bool found = false;
+// foreach (int number in numbers)
+// {
+//     total += number;
+//     if (number == 42)
+//     {
+//         found = true;
+//     }
+// }
+
+// if (found)
+// {
+//     Console.WriteLine("Set contains 42");
+// }
+// Console.WriteLine($"Total: {total}");
+//                  Using switch case
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+
+// string title = "";
+// switch (employeeLevel) {
+//     case 100:
+//     case 200:
+//     title = "Senior Associate";
+//     break;
+//     case 300:
+//     title = "Manager";
+//     break;
+//     case 400:
+//     title = "Senior Manager";
+//     break;
+//     default:
+//     title = "Associate";
+//     break;
+// }
+// Console.WriteLine($"{employeeName}, {title}");
